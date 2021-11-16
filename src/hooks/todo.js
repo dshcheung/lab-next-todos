b243748c98be7cd9c8e46ec2ae9f80500c069f6f
Denis Cheung <d.s.h.cheung@gmail.com>
2021-11-16 16:04:21 +0800

    update & destroy todo

--- src/hooks/todo.js
+++ src/hooks/todo.js
@@ -1,15 +1,38 @@
 import useSWR from 'swr'
 import axios from 'axios'
+import { useRouter } from 'next/router'
 
 const fetcher = (url) => axios.get(url).then((res) => res.data)
 
 export default function useTodo(id) {
-  const { data, error } = useSWR(id ? `https://fswdi-api-todos.herokuapp.com/api/todos/${id}` : null, fetcher)
+  const router = useRouter()
+  const { data, error, mutate } = useSWR(id ? `https://fswdi-api-todos.herokuapp.com/api/todos/${id}` : null, fetcher)
+
+  const updateTodo = (values) => {
+    axios({
+      method: 'PUT',
+      url: `https://fswdi-api-todos.herokuapp.com/api/todos/${id}`,
+      data: values
+    }).then(() => {
+      mutate()
+    })
+  }
+
+  const destroyTodo = () => {
+    axios({
+      method: 'DELETE',
+      url: `https://fswdi-api-todos.herokuapp.com/api/todos/${id}`
+    }).then(() => {
+      router.push('/swr')
+    })
+  }
 
   return {
     todo: data?.todo,
     isLoading: !error && !data,
     isError: error,
-    errorMessage: error?.response?.data?.message
+    errorMessage: error?.response?.data?.message,
+    updateTodo,
+    destroyTodo
   }
 }
--- src/pages/swr/[id].jsx
+++ src/pages/swr/[id].jsx
@@ -3,12 +3,16 @@ import useTodo from '@/hooks/todo'
 
 export default function SSGShow() {
   const { query: { id } } = useRouter()
-  const { todo, isLoading, isError, errorMessage } = useTodo(id)
+  const { todo, isLoading, isError, errorMessage, updateTodo, destroyTodo } = useTodo(id)
 
   if (isLoading) return <div>loading</div>
   if (isError) return <div>{errorMessage}</div>
   return (
     <div>
+      <div>
+        <button type="button" onClick={() => updateTodo({ title: 'denis' })}>Edit</button>
+        <button type="button" onClick={() => destroyTodo()}>Delete</button>
+      </div>
       <div>{todo.title}</div>
       <ul>
         {
