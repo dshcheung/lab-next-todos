db200c0f80f5b0c88652bfdec55c47490a5a8139
Denis Cheung <d.s.h.cheung@gmail.com>
2021-11-16 14:21:21 +0800

    swr-master

--- hooks/todos.js
+++ hooks/todos.js
@@ -1,16 +1,35 @@
 import useSWR from 'swr'
 import axios from 'axios'
+import produce from 'immer'
 
 const fetcher = url => axios.get(url).then(res => res.data)
 
 export default function useTodos() {
-  const { data, error } = useSWR('https://fswdi-api-todos.herokuapp.com/api/todos', fetcher)
+  const { data, error, mutate } = useSWR('https://fswdi-api-todos.herokuapp.com/api/todos', fetcher)
+
+  const createTodo = (values) => {
+    axios({
+      method: 'POST',
+      url: 'https://fswdi-api-todos.herokuapp.com/api/todos',
+      data: values
+    }).then((resp) => {
+      // Tell SWR to refresh the data directly
+      // mutate()
+
+      // Tell SWR to update with the response data and then
+      // revalidate the data with a get request
+      mutate(produce(data, (draft) => {
+        draft.todos.push(resp.data.todo)
+      }))
+    })
+  }
 
   return {
     meta: data?.meta,
     todos: data?.todos || [],
     isLoading: !error && !data,
     isError: error,
-    errorMessage: error?.response?.data?.message
+    errorMessage: error?.response?.data?.message,
+    createTodo
   }
 }
--- pages/swr/index.js
+++ pages/swr/index.js
@@ -3,16 +3,23 @@ import Link from 'next/link'
 import useTodos from '../../hooks/todos'
 
 export default function SWRIndex() {
-  const { todos, isLoading, isError, errorMessage } = useTodos()
+  const { todos, isLoading, isError, errorMessage, createTodo } = useTodos()
 
   if (isLoading) return <div>loading</div>
   if (isError) return <div>{errorMessage}</div>
-  if (todos.length === 0) return <div>No Todos</div>
-  return todos.map((todo) => (
-    <div key={todo.id}>
-      <Link href={`/swr/${todo.id}`}>
-        <a>{todo.title}</a>
-      </Link>
+  // if (todos.length === 0) return <div>No Todos</div>
+  return (
+    <div>
+      <button onClick={() => createTodo({ title: 'hi' })}>New Todo</button>
+      {
+        todos.map((todo) => (
+          <div key={todo.id}>
+            <Link href={`/swr/${todo.id}`}>
+              <a>{todo.title}</a>
+            </Link>
+          </div>
+        ))
+      }
     </div>
-  ))
+  )
 }
